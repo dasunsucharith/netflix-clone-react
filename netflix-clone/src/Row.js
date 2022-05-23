57854d464af03ec48c9b7fe2ef351f66427e1a52
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-05-23 12:44:55 +0530

    new

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios'
+import './Row.css'
 
 const base_url = "https://image.tmdb.org/t/p/original/"
 
@@ -24,11 +25,11 @@ function Row({ title, fetchUrl }) {
             {/* title */}
             <h2>{title}</h2>
 
-            <div className='row_posters'>
+            <div className='row__posters'>
                 {/* Several row posters */}
 
                 {movies.map(movie => (
-                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                    <img className='row__poster' src={`${base_url}${movie.poster_path}`} alt={movie.name} />
                 ))}
             </div>
             {/* container -> posters */}
