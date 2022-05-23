c26d7900a55a3c9d8a91a20bd8691bd39d98ef40
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-05-23 17:21:37 +0530

    new

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -46,6 +46,8 @@ function Banner() {
                     {truncate(movie?.overview, 150)}
                 </h1>
             </div>
+
+            <div className='banner__fadeBottom'></div>
         </header>
     )
 }
